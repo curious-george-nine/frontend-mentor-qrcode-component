75d49bf798a4ea8919259ca06c0d1fb59c337200
jestrocss <efefwefefw41@gmail.com>
2023-02-19 07:43:47 +0900

    submit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import qrCodeImage from "$lib/assets/image-qr-code.png";
+</script>
+
+<div class="w-[21rem] bg-white p-4 rounded-2xl shadow-xl">
+  <div class="pb-7">
+    <img src={qrCodeImage} alt="qr code" class="rounded-xl" />
+  </div>
+  <div class="text-center">
+    <p class="font-bold text-xl leading-8 pb-4 text-slate-800">
+      Improve your front-end skills by building projects
+    </p>
+    <p class="text-slate-500">
+      Scan the QR code to visit Frontend Mentor and take your coding skills to
+      the next level
+    </p>
+  </div>
+</div>
